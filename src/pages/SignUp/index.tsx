4fc5884d429b275cd66d934c0961cbd2df225f7c
Marlon Branco <marlonaparecido18@gmail.com>
2021-01-28 17:51:22 -0300

    Feature: Design das páginas de SignIn e SignUp finalizados

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu login</h1>
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
+
+      <a href="signup">
+        <FiArrowLeft />
+        Voltar para Logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
